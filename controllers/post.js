befaf18c792bbea4a9ee9001ae18eff737d2b6b2
CydoEntis <cydoentis@gmail.com>
2022-11-20 10:53:06 -0500

    Delete functionality added

--- controllers/post.js
+++ controllers/post.js
@@ -1,4 +1,5 @@
 import { db } from "../db.js";
+import jwt from "jsonwebtoken";
 
 const getPosts = (req, res) => {
     const category = req.query.cat;
@@ -7,7 +8,7 @@ const getPosts = (req, res) => {
         : "SELECT * FROM posts";
 
     db.query(query, [category], (err, data) => {
-        if (err) return res.json(err);
+        if (err) return res.status(500).json(err);
 
         return res.status(200).json(data);
     });
@@ -20,7 +21,7 @@ const getPost = (req, res) => {
         "SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
 
     db.query(query, [id], (err, data) => {
-        if (err) return res.json(err);
+        if (err) return res.status(500).json(err);
 
         return res.status(200).json(data[0]);
     });
@@ -31,7 +32,22 @@ const addPost = (req, res) => {
 };
 
 const deletePost = (req, res) => {
-    res.json("Post from controller");
+    const token = req.cookies.access_token;
+    if (!token) return res.status(401).json("Not authenticated");
+
+    jwt.verify(token, "jwtkey", (err, userInfo) => {
+        if (err) return res.status(403).json("Token is not valid");
+
+        const postId = req.params.id;
+        const query = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?";
+
+        db.query(query, [postId, userInfo.id], (err, data) => {
+            if (err)
+                return res.status(403).json("You can only delete your post");
+
+            return res.json(`Post with id: ${postId} has been deleted`);
+        });
+    });
 };
 
 const updatePost = (req, res) => {
