ac691f75b394b636468f1444e3f21f08d1dc6a74
CydoEntis <cydoentis@gmail.com>
2022-11-20 10:42:42 -0500

    Fixed bug with sql for fetching single post

--- controllers/post.js
+++ controllers/post.js
@@ -14,7 +14,16 @@ const getPosts = (req, res) => {
 };
 
 const getPost = (req, res) => {
-    res.json("Post from controller");
+    const id = req.params.id;
+    console.log(id);
+    const query =
+        "SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
+
+    db.query(query, [id], (err, data) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json(data[0]);
+    });
 };
 
 const addPost = (req, res) => {
